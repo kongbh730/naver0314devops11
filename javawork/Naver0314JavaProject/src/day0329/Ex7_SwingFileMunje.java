107b8a9b6b23df894a3ccb8d83d4ffccd0cb76a9
kongbh730 <kongbh730@gmail.com>
2024-04-01 10:21:19 +0900

    4월 1일

--- javawork/Naver0314JavaProject/src/day0329/Ex7_SwingFileMunje.java
+++ javawork/Naver0314JavaProject/src/day0329/Ex7_SwingFileMunje.java
@@ -31,9 +31,9 @@ public class Ex7_SwingFileMunje extends JFrame
 	JButton btnAdd, btnDelete;//사원추가, 삭제에 필요한 버튼
 	JTextField name;//사원 이름 적을 텍스트필드
 	JComboBox<String> blood, region, position;//혈액형, 지역, 직급
-	String[] comboLabelBlood = {"A", "B", "O", "AB"};//혈액형 콤보박스에 들어갈 문자열모음
-	String[] comboLabelregion = {"경기도", "경상도", "전라도", "충청도", "강원도"};//지역 콤보박스에 들어갈 문자열모음
-	String[] comboLabelPosition = {"수습", "대리", "차장", "과장", "부장", "사장"};//직급 콤보박스에 들어갈 문자열모음
+//	String[] comboLabelBlood = {"A", "B", "O", "AB"};//혈액형 콤보박스에 들어갈 문자열모음
+//	String[] comboLabelregion = {"경기도", "경상도", "전라도", "충청도", "강원도"};//지역 콤보박스에 들어갈 문자열모음
+//	String[] comboLabelPosition = {"수습", "대리", "차장", "과장", "부장", "사장"};//직급 콤보박스에 들어갈 문자열모음
 	//textfield와 textArea의 차이?
 	final static String FILENAME = "c:/naver0314/companymemberinfo.txt"; // 정보텍스트
 
@@ -43,46 +43,57 @@ public class Ex7_SwingFileMunje extends JFrame
 		super(title);
 		this.setLocation(100, 100);//시작위치
 		this.setSize(600, 600);//창크기
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//프레임 종료
-
-		//종료시 테이블의 내용을 파일에 저장 후 종료
-		this.addWindowListener(new WindowAdapter()
+		//this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//프레임 종료
+		
+		//종료시 테이블의 내용을 파일에 저장 후 종료//종료시 발생하는 이벤트
+		this.addWindowListener(new WindowAdapter()//리스너는 인터페이스, 어댑터는 내부클래스
 		{
 			@Override
 			public void windowClosing(WindowEvent e) 
 			{
 				super.windowClosing(e);
-				FileWriter fw = null;
-				try {
-					fw = new FileWriter(FILENAME);
-					for(int i = 0; i < table.getRowCount(); i++)
-					{
-						String name = table.getValueAt(i, 0).toString();
-						String blood = table.getValueAt(i, 1).toString();
-						String region = table.getValueAt(i, 2).toString();
-						String position = table.getValueAt(i, 3).toString();
-						String str = name + "," + blood + "," + region + "," + position + "\n";
-						fw.write(str);
-					}
-				} catch (IOException e1) {
-					// TODO: handle exception
-				}
-				finally
-				{
-					try {
-						fw.close();
-					} catch (IOException e1) 
-					{
-						// TODO: handle exception
-					}
-					System.exit(0);
-				}
+				sawonFileWrite();//자동저장해주는 메서드 호출
+				System.exit(0);
 			}
 		});
-		
 		this.initDesign();//각종 컴포넌트 생성
 		this.sawonFileRead();//파일 불러오기
 		this.setVisible(true);
+	}
+	
+	//종료시 파일저장
+	public void sawonFileWrite()
+	{
+		FileWriter fw = null;
+		try 
+		{
+			fw = new FileWriter(FILENAME);
+			for(int i = 0; i < table.getRowCount(); i++)
+			{
+				String s = table.getValueAt(i, 0)+","+
+						table.getValueAt(i, 1)+","+
+						table.getValueAt(i, 2)+","+
+						table.getValueAt(i, 3)+"\n";
+				fw.write(s);
+			}
+		} 
+		catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				fw.close();
+			}
+			catch (IOException e) 
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 	//파일 불러와서 테이블에 출력 
@@ -145,13 +156,15 @@ public class Ex7_SwingFileMunje extends JFrame
 
 		this.add("North", pTop);
 		//==================================================
+		
 		//====================== 중단 =======================
 		//사원정보 출력 테이블
 		String []title = {"이름","혈액형","지역","직급"};//테이블의 레이블
-		tableModel = new DefaultTableModel(title, 0);//일단은 생성만		
+		tableModel = new DefaultTableModel(title, 0);//일단은 생성만//행갯수 0개	
 		table = new JTable(tableModel);//tableModel에 맞게끔 JTable 생성
-		this.add("Center", new JScrollPane(table));//JScrollPane은 스크롤과 함께 테이블 제목이 보인다.
+		this.add("Center", new JScrollPane(table));//JScrollPane은 스크롤 + 테이블 제목이 보인다.
 		//==================================================
+		
 		//====================== 하단 =======================
 		JPanel p = new JPanel();//하단 패널
 
@@ -159,27 +172,30 @@ public class Ex7_SwingFileMunje extends JFrame
 		name = new JTextField(5);//텍스트 필드 생성
 		p.add(new JLabel("이름 : "));//패널에 레이블 추가
 		p.add(name);//패널에 name 텍스트필드 추가
-		this.add("South", p);//패널추가0
 
 		//혈액형 콤보박스
+		String[] comboLabelBlood = {"A", "B", "O", "AB"};//혈액형 콤보박스에 들어갈 문자열모음
 		blood = new JComboBox<>(comboLabelBlood);
 		p.add(new JLabel("혈액형 : "));
 		p.add(blood);
-		this.add("South", p);
 
 		//지역 콤보박스
+		String[] comboLabelregion = {"경기도", "경상도", "전라도", "충청도", "강원도"};//지역 콤보박스에 들어갈 문자열모음
 		region = new JComboBox<>(comboLabelregion);
 		p.add(new JLabel("지역 : "));
 		p.add(region);
-		this.add("South", p);
 
 		//직급 콤보박스
+		String[] comboLabelPosition = {"수습", "대리", "차장", "과장", "부장", "사장"};//직급 콤보박스에 들어갈 문자열모음
 		position = new JComboBox<>(comboLabelPosition);
 		p.add(new JLabel("직급 : "));
 		p.add(position);
-		this.add("South", p);
+		
+		//패널에 컴포넌트는 매번 추가해야 하지만
+		this.add("South", p);//패널을 추가하는 건 한번만 있으면 되지
 		//==================================================
-		//==================== 버튼 이벤트 ====================
+		
+		//==================== 버튼 이벤트 추가 ================
 		//==================== 추가 버튼 ======================
 		btnAdd.addActionListener(new ActionListener() 
 		{
@@ -188,7 +204,8 @@ public class Ex7_SwingFileMunje extends JFrame
 			{
 				String sawonName = name.getText();//이름 입력은 먼저해
 				//이름 입력이니까 뭐가 들어와도 상관 없어//입력 안한 경우만 확인해
-				if(name.getText().length() == 0)
+				//if(name.getText().length() == 0)
+				if(name.getText().equals(""))//동일한 의미!
 				{
 					//이 파일에 메세지 출력
 					JOptionPane.showMessageDialog(Ex7_SwingFileMunje.this, "이름을 입력하세요.");
@@ -196,20 +213,23 @@ public class Ex7_SwingFileMunje extends JFrame
 				}
 				//============== 이름과 콤보박스 조합 ==============
 				Vector<String> data = new Vector<String>();
-				String sawonBlood = (String) blood.getSelectedItem();//콤보박스 결과 문자열로
+				//String sawonBlood = (String) blood.getSelectedItem();//콤보박스 결과 문자열로
 				String sawonRegion = (String) region.getSelectedItem();//콤보박스 결과 문자열로
 				String sawonPosition = (String) position.getSelectedItem();//콤보박스 결과 문자열로
 
 				data.add(sawonName);//사원 이름
-				data.add(sawonBlood);//혈액형
+				//data.add(sawonBlood);//혈액형
+				data.add((String) blood.getSelectedItem());//혈액형// 이렇게 해도 됨!
 				data.add(sawonRegion);//지역
 				data.add(sawonPosition);//직급
 				//String 형태로 바꿔서 data 벡터에 추가
 
 				tableModel.addRow(data);
 				name.setText("");//텍스트필드 초기화
-				//각 콤보박스 초기화
-				
+				//각 콤보박스 초기화//이걸 왜 못찾았지
+				blood.setSelectedIndex(0);
+				region.setSelectedIndex(0);
+				position.setSelectedIndex(0);
 			}
 		});
 
