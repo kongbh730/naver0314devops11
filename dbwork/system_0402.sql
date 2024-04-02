f630d592eedc6993e6c955a66f448dbcd9d49db5
kongbh730 <kongbh730@gmail.com>
2024-04-02 11:15:32 +0900

    4월 2일

---
+++ dbwork/system_0402.sql
@@ -0,0 +1,19 @@
+-- 계정생성(system에서만 가능) : 실행은 ctrl+enter
+ create user angel identified by a1234; --오류발생
+ 
+ -- 최신버전에서는 앞에 공통문자 c##을 붙여서 계쩡생성을 해야만 한다.
+ create user c##angel identified by a1234; 
+ 
+ --system 계정에 생성된 계정 확인하기
+ select username from dba_users;
+ 
+ --c##angel 계정 삭제하기
+ drop user c##angel;
+ 
+ --c## 공통문자를 안붙이고 계정생성을 하려면 세션변경을 해야만 한다.
+ alter session set "_ORACLE_SCRIPT"=true;
+ 
+ --angel 계정 재생성(비번 a1234)
+ create user angel identified by a1234;
+ 
+ 
